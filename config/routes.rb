dc9823e45a8f1f69037cf2a563a86497125de7ea
pxai <pello_altadill@cuatrovientos.org>
2017-08-15 08:38:40 +0200

    added articles case

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'sign_in/signin'
 
+  # resources
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root 'home#index'
 end
