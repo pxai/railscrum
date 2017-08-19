9d35f1fb52b179113c05b24f7a2f1b2b3d7a99ef
pxai <pello_altadill@cuatrovientos.org>
2017-08-19 18:25:29 +0200

    last changes

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,17 +1,36 @@
 class StoriesController < ApplicationController
-  def create
-    @board = Board.find(params[:board_id])
-    @story = @board.stories.create(story_params)
-    redirect_to board_path(@board)
-  end
+    def create
+        @board = Board.find(params[:board_id])
+        @story = @board.stories.create(story_params)
+        redirect_to board_path(@board)
+    end
  
-  def show
-    @story = Story.find(params[:board_id])
-  end
-  def edit
-    @board = Board.find(params[:id])
-    @story = Story.find(params[:board_id])
-  end
+
+    def show
+        @story = Story.find(params[:board_id])
+    end
+
+    def edit
+        @board = Board.find(params[:id])
+        @story = Story.find(params[:board_id])
+    end
+
+    def update
+        @story = Story.find(params[:id])
+        if @story.update(story_params)
+            redirect_to @story
+        else
+            render 'edit'
+        end
+    end
+
+
+    def destroy
+        @story = Board.find(params[:id])
+        @story.destroy
+        redirect_to story_path
+    end
+    
   private
     def story_params
       params.require(:story).permit(:title, :body)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170818235002) do
+ActiveRecord::Schema.define(version: 20170819065128) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -38,9 +38,14 @@ ActiveRecord::Schema.define(version: 20170818235002) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
-    t.text     "text"
+    t.boolean  "completed"
+    t.text     "body"
+    t.datetime "created"
+    t.datetime "done"
+    t.integer  "story_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["story_id"], name: "index_tasks_on_story_id"
   end
 
 end
