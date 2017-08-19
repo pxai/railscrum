6458007f1f1db9880c5c83346436706bca78ba75
pxai <pello_altadill@cuatrovientos.org>
2017-08-19 08:52:00 +0200

    Ok, added task model, have to add --force at the end of generation

--- db/migrate/20170819065128_create_tasks.rb
+++ db/migrate/20170819065128_create_tasks.rb
@@ -2,7 +2,11 @@ class CreateTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.text :text
+      t.boolean :completed
+      t.text :body
+      t.datetime :created
+      t.datetime :done
+      t.references :story, foreign_key: true
 
       t.timestamps
     end
