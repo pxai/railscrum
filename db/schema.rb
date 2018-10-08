7f4efe324b29fc09886dc9eefa9dd1dc1d69047b
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-10-08 16:37:18 +0200

    minor fixes

--- db/schema.rb
+++ db/schema.rb
@@ -33,13 +33,6 @@ ActiveRecord::Schema.define(version: 20170825151343) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "msgs", force: :cascade do |t|
-    t.string   "msg"
-    t.datetime "sent"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "stories", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
